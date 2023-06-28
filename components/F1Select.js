282592c5e2373477ef7be576041d667c66879a4c
colin.chamabchan@gmail.com <colin.chambachan@gmail.com>
2023-06-28 11:43:53 -0400

    update styles

--- components/F1Select.js
+++ components/F1Select.js
@@ -1,15 +1,24 @@
-const F1Select = ({ id, options }) => {
+import { useEffect } from "react";
+
+const F1Select = ({ id, options, setValue = () => "" }) => {
   return (
     <div className="px-5">
-      <select className=" bg-red-700 text-white w-72 mb-6 text-sm rounded-lg focus:ring-white focus:border-white block  p-2.5  dark:border-gray-600 dark:placeholder-gray-400">
-        <option selected className="text-gray-700 bg-slate-100">
+      <select
+        onClick={(e) => {
+          setValue(e.target.value);
+        }}
+        className=" bg-red-700 text-white w-72 mb-6 text-sm rounded-lg focus:ring-white focus:border-white block  p-2.5  dark:border-gray-600 dark:placeholder-gray-400"
+      >
+        <option defaultValue className="text-gray-700 bg-slate-100">
           Choose a {id}
         </option>
         {options?.map((element) => {
           return (
             <option
+              key={element.value}
               className="text-gray-700 bg-slate-100"
               value={element.value}
+              label={element.label}
             >
               {element.label}
             </option>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center flex-row w-full h-12 font-semibold lg:text-4xl text-2xl  px-10 lg:px-16 xl:px-72 mt-5  justify-between">
-      <a className="flex items-center" href="/">
+    <div className="flex items-center flex-row w-full h-12 font-semibold lg:text-4xl text-2xl  px-10 lg:px-16 xl:px-72 pt-5  justify-between">
+      <Link className="flex items-center" href="/">
         <Image
           src="/assets/images/F1.png"
           className="float-left mr-2"
@@ -13,7 +14,8 @@ const Navbar = () => {
         />
 
         <span>F1 Archive</span>
-      </a>
+      </Link>
+
       <a
         type="button"
         className="flex hover:cursor-pointer bg-white h-8 lg:h-10 items-center rounded-l-full rounded-r-full text-sm lg:text-lg px-4 py-2 text-neutral-800 hover:scale-105 transition ease-in-out "
